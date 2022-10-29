68e7ac50dbaa328e78925a4978f689b7488714e9
Your Name <you@example.com>
2022-10-29 11:12:18 +0100

    Renamed dependant files

--- SpecFlowProject2/Features/CompareTemperatures.feature.cs
+++ SpecFlowProject2/Features/CompareTemperatures.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace SpecFlowProject2.Features
+namespace VitalityExercise.Features
 {
     using TechTalk.SpecFlow;
     using System;
